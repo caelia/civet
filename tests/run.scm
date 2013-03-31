1463a89010fa768c9cae91c0ee0586e08f1c1e26
mgushee <matt@gushee.net>
2013-03-30 21:03:19 -0600

    Fixed errors and numbering inconsistencies in test script.

--- tests/run.scm
+++ tests/run.scm
@@ -287,16 +287,36 @@ XML
         (cvt "http://xmlns.therebetygers.net/civet/0.1")))
    (html (@ (xml:lang "en") (lang "en"))
          (head (title))
-         (body (div (@ (cvt:class "divclass")) "Div Contents")))))
+         (body
+           (cvt:if
+             (@ (test "color"))
+             (p (cvt:var (@ (name "size"))))
+             (p (cvt:var (@ (name "age")))))))))
 
 (define doc-tr1-10-out
 '(*TOP* (@ (*NAMESPACES* (#f "http://www.w3.org/1999/xhtml")
         (cvt "http://xmlns.therebetygers.net/civet/0.1")))
    (html (@ (xml:lang "en") (lang "en"))
          (head (title))
-         (body (div (@ (class "Poinsettia")) "Div Contents")))))
+         (body
+           (p "12")
+           (p "27")))))
 
 (define doc-tr1-11-in
+'(*TOP* (@ (*NAMESPACES* (#f "http://www.w3.org/1999/xhtml")
+        (cvt "http://xmlns.therebetygers.net/civet/0.1")))
+   (html (@ (xml:lang "en") (lang "en"))
+         (head (title))
+         (body (div (@ (cvt:class "divclass")) "Div Contents")))))
+
+(define doc-tr1-11-out
+'(*TOP* (@ (*NAMESPACES* (#f "http://www.w3.org/1999/xhtml")
+        (cvt "http://xmlns.therebetygers.net/civet/0.1")))
+   (html (@ (xml:lang "en") (lang "en"))
+         (head (title))
+         (body (div (@ (class "Poinsettia")) "Div Contents")))))
+
+(define doc-tr1-12-in
 '(*TOP* (@ (*NAMESPACES* (#f "http://www.w3.org/1999/xhtml")
         (cvt "http://xmlns.therebetygers.net/civet/0.1")))
    (html (@ (xml:lang "en") (lang "en"))
@@ -309,7 +329,7 @@ XML
              (cvt:attr (@ (value "fedora") (name "chapeau")))
              "Div Contents")))))
 
-(define doc-tr1-11-out
+(define doc-tr1-12-out
 '(*TOP* (@ (*NAMESPACES* (#f "http://www.w3.org/1999/xhtml")
         (cvt "http://xmlns.therebetygers.net/civet/0.1")))
    (html (@ (xml:lang "en") (lang "en"))
@@ -319,7 +339,7 @@ XML
              (@ (class "Poinsettia") (id "baz451") (chapeau "fedora"))
              "Div Contents")))))
 
-(define ctx-tr1-12
+(define ctx-tr1-13
   (make-context state: 'init
                 nsmap: '((#f . "http://www.w3.org/1999/xhtml")
                          (cvt . "http://xmlns.therebetygers.net/civet/0.1"))
@@ -338,15 +358,18 @@ XML
                                     ((title . "The Left Hand of Darkness")
                                      (author . "Ursula K. LeGuin")
                                      (desc . "An interplanetary diplomat has an eye-opening encounter with a humanoid race whose people switch gender according to the situation."))
-                                    ((title . "The War at the End of the World")
+                                    ((title . "The War of the End of the World")
                                      (author . "Mario Vargas Llosa")
                                      (desc . "A band of outcasts try to build a utopian community deep in the Amazon jungle, only to be slaughtered by the Brazilian army."))
                                     ((title .  "Disaster Capitalism")
                                      (author . "Naomi Klein")
-                                     (desc . "The author explains how the global elite take advantage of natural and political crises to advance the neoliberal economic agenda.")))))))
+                                     (desc . "The author explains how the global elite take advantage of natural and political crises to advance the neoliberal economic agenda."))))
+                         (ein-buch . (((title . "A Study in Scarlet")
+                                       (author . "Sir Arthur Conan Doyle")
+                                       (desc . "In which we encounter the extraordinary &amp; eccentric Sherlock Holmes for the first time.")))))))
 
 
-(define doc-tr1-12-in
+(define doc-tr1-13-in
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -363,7 +386,7 @@ XML
             (@ (name "x")))
           (br))))))
 
-(define doc-tr1-12-out
+(define doc-tr1-13-out
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -373,16 +396,16 @@ XML
     (@ (xml:lang "en") (lang "en"))
     (head (title))
       (body
-        "gecko"
+        "chameleon"
         (br)
-        "komodo dragon"
+        "gecko"
         (br)
         "gila monster"
         (br)
-        "chameleon"
+        "komodo dragon"
         (br)))))
 
-(define doc-tr1-13-in
+(define doc-tr1-14-in
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -393,12 +416,12 @@ XML
     (head (title))
       (body 
         (cvt:for
-          (@ (in "books") (each "book"))
+          (@ (in "books") (each "book") (sort "none"))
           (p
             (cvt:var
               (@ (name "book")))))))))
 
-(define doc-tr1-13-out
+(define doc-tr1-14-out
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -414,7 +437,7 @@ XML
         (p "One Hundred Years of Solitude")
         (p "Disaster Capitalism")))))
 
-(define doc-tr1-14-in
+(define doc-tr1-15-in
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -443,7 +466,7 @@ XML
                 (cvt:var
                   (@ (name "libro.desc")))))))))))
 
-(define doc-tr1-14-out
+(define doc-tr1-15-out
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -464,9 +487,9 @@ XML
               (td "Sir Arthur Conan Doyle"))
             (tr
               (th "Description")
-              (td "In which we encounter the extraordinary & eccentric Sherlock Holmes for the first time."))))))))
+              (td "In which we encounter the extraordinary &amp; eccentric Sherlock Holmes for the first time."))))))))
 
-(define doc-tr1-15-in
+(define doc-tr1-16-in
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -476,8 +499,8 @@ XML
     (@ (xml:lang "en") (lang "en"))
     (head (title))
       (body 
-        (for
-          (@ (in "livres") (each "livre"))
+        (cvt:for
+          (@ (in "livres") (each "livre") (sort-field "title"))
           (div
             (@ (class "bookinfo"))
             (table
@@ -491,7 +514,7 @@ XML
                 (th "Description")
                 (td (cvt:var (@ (name "livre.desc"))))))))))))
 
-(define doc-tr1-15-out
+(define doc-tr1-16-out
 '(*TOP*
   (@
     (*NAMESPACES*
@@ -501,6 +524,17 @@ XML
     (@ (xml:lang "en") (lang "en"))
     (head (title))
       (body 
+        (div (@ (class "bookinfo"))
+          (table
+            (tr
+              (th "Title")
+              (td "Disaster Capitalism"))
+            (tr
+              (th "Author")
+              (td "Naomi Klein"))
+            (tr
+              (th "Description")
+              (td "The author explains how the global elite take advantage of natural and political crises to advance the neoliberal economic agenda."))))
         (div
           (@ (class "bookinfo"))
           (table
@@ -517,17 +551,6 @@ XML
           (table
             (tr
               (th "Title")
-              (td "The Wise Man's Fear"))
-            (tr
-              (th "Author")
-              (td "Patrick Rothfuss"))
-            (tr
-              (th "Description")
-              (td "The second volume in the story of Kvothe, a world-destroying wizard."))))
-        (div (@ (class "bookinfo"))
-          (table
-            (tr
-              (th "Title")
               (td "The Left Hand of Darkness"))
             (tr
               (th "Author")
@@ -539,7 +562,7 @@ XML
           (table
             (tr
               (th "Title")
-              (td "The War at the End of the World"))
+              (td "The War of the End of the World"))
             (tr
               (th "Author")
               (td "Mario Vargas Llosa"))
@@ -550,13 +573,61 @@ XML
           (table
             (tr
               (th "Title")
-              (td "Disaster Capitalism"))
+              (td "The Wise Man's Fear"))
             (tr
               (th "Author")
-              (td "Naomi Klein"))
+              (td "Patrick Rothfuss"))
             (tr
               (th "Description")
-              (td "The author explains how the global elite take advantage of natural and political crises to advance the neoliberal economic agenda."))))))))
+              (td "The second volume in the story of Kvothe, a world-destroying wizard."))))))))
+
+(define doc-tr1-17-in
+'(*TOP*
+  (@
+    (*NAMESPACES*
+      (#f "http://www.w3.org/1999/xhtml")
+      (cvt "http://xmlns.therebetygers.net/civet/0.1")))
+  (html
+    (@ (xml:lang "en") (lang "en"))
+    (head (title))
+      (body 
+        (cvt:for
+          (@ (in "ein-buch") (each "buch") (sort-field "title"))
+          (div
+            (@ (class "bookinfo"))
+            (table
+              (tr
+                (th "Title")
+                (td (cvt:var (@ (name "buch.title")))))
+              (tr
+                (th "Author")
+                (td (cvt:var (@ (name "buch.author")))))
+              (tr
+                (th "Description")
+                (td (cvt:var (@ (name "buch.desc"))))))))))))
+
+(define doc-tr1-17-out
+'(*TOP*
+  (@
+    (*NAMESPACES*
+      (#f "http://www.w3.org/1999/xhtml")
+      (cvt "http://xmlns.therebetygers.net/civet/0.1")))
+  (html
+    (@ (xml:lang "en") (lang "en"))
+    (head (title))
+      (body 
+        (div
+          (@ (class "bookinfo"))
+          (table
+            (tr
+              (th "Title")
+              (td "A Study in Scarlet"))
+            (tr
+              (th "Author")
+              (td "Sir Arthur Conan Doyle"))
+            (tr
+              (th "Description")
+              (td "In which we encounter the extraordinary &amp; eccentric Sherlock Holmes for the first time."))))))))
 
 ;;; ========================================================================
 ;;; ------  Run tests  -----------------------------------------------------
@@ -599,29 +670,37 @@ XML
     doc-tr1-9-out
     (process-base-template doc-tr1-9-in '() ctx-tr1-4))
   (test
-    "TR1.10: direct attribute substitution"
+    "TR1.10: cvt:if, insert two vars, each in its own paragraph"
     doc-tr1-10-out
     (process-base-template doc-tr1-10-in '() ctx-tr1-4))
   (test
-    "TR1.11: attribute substitution using cvt:attr elements"
+    "TR1.11: direct attribute substitution"
     doc-tr1-11-out
     (process-base-template doc-tr1-11-in '() ctx-tr1-4))
   (test
-    "TR1.12: cvt:for, insert text nodes"
+    "TR1.12: attribute substitution using cvt:attr elements"
     doc-tr1-12-out
-    (process-base-template doc-tr1-12-in '() ctx-tr1-12))
+    (process-base-template doc-tr1-12-in '() ctx-tr1-4))
   (test
-    "TR1.13: cvt:for, insert text in a <p>"
+    "TR1.13: cvt:for, insert text nodes"
     doc-tr1-13-out
-    (process-base-template doc-tr1-13-in '() ctx-tr1-12))
+    (process-base-template doc-tr1-13-in '() ctx-tr1-13))
   (test
-    "TR1.14: cvt:var with object variable"
+    "TR1.14: cvt:for, unsorted, insert text in a <p>"
     doc-tr1-14-out
-    (process-base-template doc-tr1-14-in '() ctx-tr1-12))
+    (process-base-template doc-tr1-14-in '() ctx-tr1-13))
   (test
-    "TR1.15: cvt:for, insert object variable"
+    "TR1.15: cvt:var with object variable"
     doc-tr1-15-out
-    (process-base-template doc-tr1-15-in '() ctx-tr1-12)))
+    (process-base-template doc-tr1-15-in '() ctx-tr1-13))
+  (test
+    "TR1.16: cvt:for with sort-field, inserting object fields"
+    doc-tr1-16-out
+    (process-base-template doc-tr1-16-in '() ctx-tr1-13))
+  (test
+    "TR1.17: cvt:for over a one-item list, inserting object fields"
+    doc-tr1-17-out
+    (process-base-template doc-tr1-17-in '() ctx-tr1-13)))
 
 ;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 
